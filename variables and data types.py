af3ba9bb11a93b6c41f3a3bb35beb933bb674510
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-13 09:15:29 +0100

    Update variables and data types.py

--- variables and data types.py
+++ variables and data types.py
@@ -1,6 +1,12 @@
+#this shows a string
 character_name = "John"
+##This shows an int
 character_age = "35"
+##this shows a boolian
+is_male = True
+
 
 print("My name is " + character_name + " and my age is " + character_age)
+
 
 
