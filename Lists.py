cf31aba8b9d34a4adf19ce011efe931ccea6e7f4
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-14 11:36:18 +0100

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,6 +1,17 @@
 #this is how to create a list
-this_is_a_list = ["Callum", "Ernie", "Jake"]
+this_is_a_list = ["Callum", "Ernie", "Jake", "Kirsty"]
 #printing a list
 print(this_is_a_list)
 #this is how to print an idex position
 print(this_is_a_list[0])
+
+#this will print at the back of the list
+print(this_is_a_list[-1])
+
+#this will print of a range of elements in the list depending on the idex position
+print(this_is_a_list[1:3])
+
+#this can be used to modify the elements of a list using index position
+this_is_a_list[0] = "George"
+
+print(this_is_a_list)
