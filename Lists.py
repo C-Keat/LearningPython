df18c99407c6d101aef3ccd7a111b3d967724ba2
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-14 11:32:32 +0100

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,6 @@
+#this is how to create a list
+this_is_a_list = ["Callum", "Ernie", "Jake"]
+#printing a list
+print(this_is_a_list)
+#this is how to print an idex position
+print(this_is_a_list[0])
