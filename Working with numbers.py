3ce10570a84a3043075deda09fce68a623e4dcc1
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-14 09:42:09 +0100

    Working with numbers examples

---
+++ Working with numbers.py
@@ -0,0 +1,42 @@
+from math import *
+#this is how you import external code
+
+#this is how to print numbers
+print(-2.07)
+#this is how to do maths with numbers
+print(3*4.5)
+
+print(3 * (4 + 5))
+
+#this is the mod, it devides the 10 by 3 then returns the remainder of that, which would be 1 in this case, what ever is left is printed
+print(10 % 3)
+
+#how a number variable works
+my_num = 5
+
+print(my_num)
+#this is how to convert to a number to a string
+print(str(my_num) + " my fav number")
+
+my_num = -5
+#this is an absolute function, that turns a negative number to a possitve one
+print(abs(my_num))
+
+#this is power 2, this will do 5 to the power of 2
+print(pow(5, 2))
+
+#prints out the largest of a range
+print(max(6, 4))
+#prints out the smallest
+print(min(6, 3))
+
+#round allows us to round to the nearest whole number
+print(round(7.5))
+
+#grabs the lowest number
+print(floor(3.7))
+#forces a round up
+print(ceil(3.7))
+
+#this gives the square root of a number
+print(sqrt(36))
