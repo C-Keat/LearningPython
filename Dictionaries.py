005d27f0e16a9e719ac09aa51b3f0c0fa82e45e6
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 10:25:15 +0100

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,18 @@
+#Dictionaries are special stuctures in python which allows us to store information in what are called key value paires
+
+#how to create a dictionary (Keys must be unique)
+monthConverstions = {
+    #defining the keys "KEY": "Value"
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
