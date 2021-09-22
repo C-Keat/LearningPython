4f7df76defc0350370228edac1126cf48318a1fb
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 08:56:51 +0100

    Tuples and Functions added

---
+++ Functions.py
@@ -0,0 +1,5 @@
+#How to create a function
+def say_hi():
+    print("Hi")
+#How to call a function
+say_hi()
---
+++ Tuples.py
@@ -0,0 +1,12 @@
+#A tuple is a type of data stucture - which means its a container to store values
+# Tuples are like lists but they do have differences
+# lists can be changed, Tuples cant be changed, Most of the time you use tuples if the data doesnt need to be changed
+# only so mistakes dont have to happen
+
+# Tubles are inmutible tuples cant be changed or modifide
+
+# How to create a tuple
+coordinates = (4, 5)
+print(coordinates[0])
+
+
