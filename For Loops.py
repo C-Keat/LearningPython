864609ebda07c9c4349df0a951c619679ee04aa3
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 11:16:48 +0100

    Create For Loops.py

---
+++ For Loops.py
@@ -0,0 +1,19 @@
+#how to create a for loop - this for loop prints out each letter in the ""
+#this allows you to do somthing based on the length of letters
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Jim", "Karen", "Kevin"]
+for friends in friends:
+    print(friends)
+
+#this performs an action for the number of times in the range
+for index in range(10):
+    print(index)
+
+len(friends)
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(10):
+    print("I will perform this 10 times: " + index)
