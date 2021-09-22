1307e60a8fb7f680b59df30da69687eda0cca321
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 13:19:43 +0100

    Create Build a translator.py

---
+++ Build a translator.py
@@ -0,0 +1,15 @@
+#this program changes any vowle in the english language will be turned into a G
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Input here: ")))
