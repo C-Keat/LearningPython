65253fa75533dff3502c7c475388bb5882933932
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 13:42:29 +0100

    Reading files added

---
+++ Reading files.py
@@ -0,0 +1,8 @@
+#this allows you to open a file in a read function, it will only allow you to read the file and not change anything
+open("employees.txt", "r")
+
+#this allows you to open the file in a write function, it will allow you to modify the file
+#open("employees.txt", "w")
+
+#this will allow you to open the file as an append, it will allow you to append to the end of the file
+#open("employees.txt", "a")
