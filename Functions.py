5248baf5a1ac393308eb35a76fa97301c6a6ba53
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 08:59:59 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -3,3 +3,9 @@ def say_hi():
     print("Hi")
 #How to call a function
 say_hi()
+
+#Function with a paramater
+def say_hi_par(name):
+    print(name)
+#calling function with paramater
+say_hi_par("Mike")
