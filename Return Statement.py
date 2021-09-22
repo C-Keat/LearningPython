4e340174ae0e0fdab6f03741f621d0b71a5ea195
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-22 09:14:13 +0100

    Update Return Statement.py

---
+++ Return Statement.py
@@ -0,0 +1,10 @@
+#A return statment is a function that returns a value like a maths problem
+#how to create a return for functions
+def cube(num):
+    return num*num*num
+#how to print what is returned from a fuction
+print(cube(3))
+
+#how to assign a return value to a variable
+result = cube(4)
+print(result)
