54fb686892510cbcba9f29d71c3e2b066560d889
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-23 14:25:04 +0100

    Update Moduels & pips.py

--- Modules & pips/Moduels & pips.py
+++ Modules & pips/Moduels & pips.py
@@ -5,3 +5,8 @@ import useful_tools
 
 #how to call the functions from another file
 print(useful_tools.roll_dice(10))
+
+
+
+#pip is a program that is pre installed, it can be used to install moduels that are not already included inside of python
+# it can be considered a package manager - this basically allows you to manage(unistall and install moduals)
