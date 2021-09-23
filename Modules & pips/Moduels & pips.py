74c46cc2f5e50c1031959ffe35a8b8448556e225
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-23 14:02:58 +0100

    Adding files needed for moduels&pips

---
+++ Modules & pips/Moduels & pips.py
@@ -0,0 +1,2 @@
+#moduels are basically python files that you can input into the current python file - if there was a python file
+#that had functions and variables on, if you inport that file you could use them in your current file
