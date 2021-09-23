8b67e7df5060d2db9c3aa9bd8059c8336d1a4b27
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-23 15:49:56 +0100

    Changes to the Chinese chef and run file

--- Inheritance/ChineseChef.py
+++ Inheritance/ChineseChef.py
@@ -1,3 +1,12 @@
-#this Chinese Chef can do everything that the chef can do
+#this Chinese Chef can do everything that the chef can do and will inherit from the chef class
+#this is how to perform inheritance
+from Chef import Chef
+class ChineseChef(Chef):
 
-class ChineseChef:
+    def make_fried_rice(self):
+        print("Chef makes fried rice")
+
+#Polymothafis takes place by replacing the same function name with different actions
+    def make_special_dish(self):
+        print("chef makes yellow chicken")
+
--- Inheritance/Inheritance_run_file.py
+++ Inheritance/Inheritance_run_file.py
@@ -1,4 +1,8 @@
 from Chef import Chef
+from ChineseChef import ChineseChef
 
 myChef = Chef()
 myChef.make_chicken()
+
+my_chinese_chef = ChineseChef()
+my_chinese_chef.make_special_dish()
