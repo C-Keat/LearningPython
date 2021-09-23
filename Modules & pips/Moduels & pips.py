701b6b2a970f7bcc0dcc4ff23e1f443e995c1973
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-23 14:17:01 +0100

    Update Moduels & pips.py

--- Modules & pips/Moduels & pips.py
+++ Modules & pips/Moduels & pips.py
@@ -2,5 +2,6 @@
 #that had functions and variables on, if you inport that file you could use them in your current file
 #this is how to import other python files
 import useful_tools
+
 #how to call the functions from another file
 print(useful_tools.roll_dice(10))
