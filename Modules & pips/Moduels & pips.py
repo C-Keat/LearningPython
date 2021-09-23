ad9817c507be494301b6fcfb43a429a1a1afbe76
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-23 14:09:02 +0100

    Update to moduals and pips

--- Modules & pips/Moduels & pips.py
+++ Modules & pips/Moduels & pips.py
@@ -1,2 +1,6 @@
 #moduels are basically python files that you can input into the current python file - if there was a python file
 #that had functions and variables on, if you inport that file you could use them in your current file
+#this is how to import other python files
+import useful_tools
+#how to call the functions from another file
+print(useful_tools.roll_dice(10))
