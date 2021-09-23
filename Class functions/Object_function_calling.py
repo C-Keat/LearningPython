654afec0e50f9f21db2153c2c0ee7d60912ed7c8
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-09-23 15:29:12 +0100

    Update Object_function_calling.py

--- Class functions/Object_function_calling.py
+++ Class functions/Object_function_calling.py
@@ -2,7 +2,9 @@
 #from "file name" import "class name"
 from Student import Student
 
+#creating objects form a class
 student1 = Student("Oscar", "Accounting", 3.1)
 student2 = Student("Phyllis", "Business", 3.8)
 
+#print the output of a function in an object
 print(student2.on_honor_roll())
